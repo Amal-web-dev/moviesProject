a56a832a9e2767e2ab53bc829c1ef87308894c2c
Amal <amalabdurahmonov427@gmail.com>
2023-05-16 13:13:20 +0300

    second day

--- project/js/script.js
+++ project/js/script.js
@@ -36,15 +36,40 @@ let IMBD = document.querySelector('.IMBD')
 let kino = document.querySelector('.kino')
 let typeMovie = document.querySelectorAll('.promo__menu-item')
 let click = false
+let promoMenu = document.querySelector('.ul-menu')
+let genres = []
+let gen = []
+let isGenresAdded = false;
+let bgRate = document.querySelector('.bg-rate')
+let modulRate = document.querySelector('.modul-rate')
+let closeRate = document.querySelector('.img-close')
+let search = document.querySelector('#search')
+let searchIcon = document.querySelector('.search-icon')
 
-function reload(arr, place) {
+search.onkeyup = () => {
+  let value = search.value.toLowerCase().trim()
+  let filtered = movies.filter(movie => {
+  let title = movie.Title.toLowerCase().trim()
+    if(title.includes(value)){
+      return movie
+    }
+  })
+
+  reload(filtered, listP)
+}
+
+  function reload(arr, place) {
+
   place.innerHTML = "";
 
-  arr.sort((a, b) => a.Title.localeCompare(b.Title));
 
+
+  arr.sort((a, b) => a.Title.localeCompare(b.Title));
   for (const item of arr) {
     let p = document.createElement("li");
     let div = document.createElement("div");
+    let poster = document.createElement('img')
+    let rating = document.createElement('p')
 
     p.classList.add("promo__interactive-item");
     div.classList.add("delete");
@@ -55,22 +80,71 @@ function reload(arr, place) {
     place.append(p);
     p.append(div);
 
+
     p.onclick = () => {
       bg.style.backgroundImage = 'url(' + item.Poster + ')'
+    poster.src = item.Poster
+    poster.style.height = '400px'
+    rating.innerHTML = 'IMBD: ' + item.imdbRating
       pGenre.innerHTML = item.Genre
       adsName.innerHTML = item.Title 
       decr.innerHTML = item.Plot
       IMBD.innerHTML = 'IMBD: ' + item.imdbRating
       kino.innerHTML = 'Кинопоиск: ' +  Math.round(Math.random() * 3 + 7)
+      bgRate.style.display = 'block'
+      modulRate.style.display = 'block'
     }
+
+    modulRate.append(poster, rating)
+
+
+    bgRate.onclick = () => {
+      bgRate.style.display = 'none'
+      modulRate.style.display = 'none'
+    }
+
+    closeRate.onclick = () => {
+      bgRate.style.display = 'none'
+      modulRate.style.display = 'none'
+    }
+
+    
 
     div.onclick = () => {
       p.remove();
     }
-    
-
+    genres.push(item.Genre)
   }
+
+  promoMenu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('promo__menu-item')) {
+      const filteredMovies = movies.filter(movie => movie.Genre.includes(event.target.innerHTML));
+      reload(filteredMovies, listP);
+    }
+  });
+
+
+  if (!isGenresAdded) {
+    gen = [...new Set(genres)];
+
+    for (const el of gen) {
+      let li = document.createElement("li");
+
+      li.classList.add('promo__menu-item')
+      li.innerHTML = el
+
+      promoMenu.append(li)
+    }
+
+    isGenresAdded = true;
+  }
+  
+
 }
+
+
+
+
 
 const listP = document.querySelector('.promo__interactive-list');
 reload(movies, listP);
@@ -110,6 +184,7 @@ typeMovie.forEach(movie => {
 
 let adv = document.querySelectorAll('#adv-logo')
 let close = document.querySelector('.icon-close')
+
 
 close.onclick = () => {
   adv.forEach(addvert => {
